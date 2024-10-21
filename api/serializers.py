d6ede0f10db2ca83ba04e5f7a2f3345b144eecff
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-10-21 16:50:32 +0500

    zxcz

--- api/serializers.py
+++ api/serializers.py
@@ -5,5 +5,5 @@ class RecognizeSerializer(serializers.Serializer):
     image1 = serializers.ImageField()
     image2 = serializers.ImageField()
 
-    image1_path = serializers.CharField()
-    image2_path = serializers.CharField()
+    # image1_path = serializers.CharField()
+    # image2_path = serializers.CharField()
